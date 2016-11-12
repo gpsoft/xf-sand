61c054b636e3a24a9dba01413ae2bcff7ba5d02c
gpsoft <etoh.gpsoft@gmail.com>
2016-11-12 12:08:52 +0900

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to xf-sand
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/xf_sand/core_test.clj
@@ -0,0 +1,7 @@
+(ns xf-sand.core-test
+  (:require [clojure.test :refer :all]
+            [xf-sand.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
